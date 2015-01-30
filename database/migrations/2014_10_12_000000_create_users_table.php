8b1b7ae01ebb90dec3b2ca8eea8bfd122436e04c
steve <steve.axtmann@gmail.com>
2015-01-30 18:34:33 +0100

    updated users migration slightly

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,9 +15,12 @@ class CreateUsersTable extends Migration {
 		Schema::create('users', function(Blueprint $table)
 		{
 			$table->increments('id');
-			$table->string('name');
+			$table->string('username')->unique();
 			$table->string('email')->unique();
 			$table->string('password', 60);
+			$table->string('avatar');
+			$table->string('confirmation_code');
+			$table->boolean('confirmed');
 			$table->rememberToken();
 			$table->timestamps();
 		});
