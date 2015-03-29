43fe583a2b67f6343d6f2ca5816f4fb0d9aa9ee1
steve <steve.axtmann@gmail.com>
2015-03-29 13:56:56 +0200

    #1 fixed routes

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,31 +1,9 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the controller to call when that URI is requested.
-|
-*/
-
-// examples
-Route::get('/', 'HomeController@index');
-Route::get('error', 'HomeController@badRequest');
-Route::get('transform', 'HomeController@transformExample');
-Route::get('transformItem', 'HomeController@transformItemExample');
-// examples end
-
-Route::group(['prefix' => 'auth', 'namespace' => 'Auth'], function () {
-    Route::post('login', 'AuthController@login');
-    Route::post('register', 'AuthController@register');
-    Route::post('confirm', 'AuthController@confirm');
-    Route::post('resetPassword', 'AuthController@resetPassword');
-    Route::post('resetPasswordComplete', 'AuthController@resetPasswordComplete');
-    Route::get('logout', 'AuthController@logout');
-});
+Route::controllers([
+    'auth' => 'Auth\AuthController',
+    'password' => 'Auth\PasswordController',
+]);
 
 Route::group(['prefix' => 'admin'], function () {
     Route::group(['namespace' => 'Permissions'], function () {
