9e44009b2655a3879a428b07c87f8415dfb42d80
steve <steve.axtmann@gmail.com>
2015-03-29 13:50:48 +0200

    #1 fixed use statements

--- app/Http/Controllers/Auth/PasswordController.php
+++ app/Http/Controllers/Auth/PasswordController.php
@@ -1,6 +1,6 @@
 <?php namespace TagProNews\Http\Controllers\Auth;
 
-use App\Http\Controllers\Controller;
+use TagProNews\Http\Controllers\Controller;
 use Illuminate\Contracts\Auth\Guard;
 use Illuminate\Contracts\Auth\PasswordBroker;
 use Illuminate\Foundation\Auth\ResetsPasswords;
