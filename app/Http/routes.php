3cd8c31a72bf84f82ee068260b572bdc7b88586f
steve <steve.axtmann@gmail.com>
2015-04-02 22:39:28 +0200

    added test route

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,5 +1,9 @@
 <?php
 
+Route::get('/', function(){
+    return 'Hi, welcome to TagProNews!';
+});
+
 Route::controllers([
     'auth' => 'Auth\AuthController',
     'password' => 'Auth\PasswordController',
