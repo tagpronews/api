03e644c072e6d7e9638a92dd1981a119c2b3f883
Steve <steve.axtmann@gmail.com>
2015-05-12 21:57:43 +0200

    converted registration form to be the same style as the login

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -3,63 +3,48 @@
 @section('content')
 <div class="container-fluid" style="background-color:#DF3442;padding-bottom: 150px;">
 	<div class="row">
-		<div class="col-md-8 col-md-offset-2">
-			<div class="panel panel-default">
-				<div class="panel-heading">Register</div>
-				<div class="panel-body">
-					@if (count($errors) > 0)
-						<div class="alert alert-danger">
-							<strong>Whoops!</strong> There were some problems with your input.<br><br>
-							<ul>
-								@foreach ($errors->all() as $error)
-									<li>{{ $error }}</li>
-								@endforeach
-							</ul>
-						</div>
-					@endif
+        <div style="width:300px;margin: 0 auto">
+            <div class="login-form-avatar">ME</div>
+                @if (count($errors) > 0)
+                    <div class="alert alert-danger">
+                        <strong>Whoops!</strong> There were some problems with your input.<br><br>
+                        <ul>
+                            @foreach ($errors->all() as $error)
+                                <li>{{ $error }}</li>
+                            @endforeach
+                        </ul>
+                    </div>
+                @endif
 
-					<form class="form-horizontal" role="form" method="POST" action="{{ url('/auth/register') }}">
-						<input type="hidden" name="_token" value="{{ csrf_token() }}">
+                <form class="form-horizontal" role="form" method="POST" action="{{ url('/auth/register') }}">
+                    <input type="hidden" name="_token" value="{{ csrf_token() }}">
 
-						<div class="form-group">
-							<label class="col-md-4 control-label">Name</label>
-							<div class="col-md-6">
-								<input type="text" class="form-control" name="name" value="{{ old('name') }}">
-							</div>
-						</div>
+                    <div class="form-group">
+                        <input type="text" class="form-control" name="name" value="{{ old('name') }}" placeholder="Username">
+                    </div>
 
-						<div class="form-group">
-							<label class="col-md-4 control-label">E-Mail Address</label>
-							<div class="col-md-6">
-								<input type="email" class="form-control" name="email" value="{{ old('email') }}">
-							</div>
-						</div>
+                    <div class="form-group">
+                        <input type="password" class="form-control" name="password" placeholder="Password">
+                    </div>
 
-						<div class="form-group">
-							<label class="col-md-4 control-label">Password</label>
-							<div class="col-md-6">
-								<input type="password" class="form-control" name="password">
-							</div>
-						</div>
+                    <div class="form-group">
+                        <input type="password" class="form-control" name="password_confirmation" placeholder="Confirm Password">
+                    </div>
 
-						<div class="form-group">
-							<label class="col-md-4 control-label">Confirm Password</label>
-							<div class="col-md-6">
-								<input type="password" class="form-control" name="password_confirmation">
-							</div>
-						</div>
+                    <div class="form-group">
+                        <input type="email" class="form-control" name="email" value="{{ old('email') }}" placeholder="Email">
+                    </div>
 
-						<div class="form-group">
-							<div class="col-md-6 col-md-offset-4">
-								<button type="submit" class="btn btn-primary">
-									Register
-								</button>
-							</div>
-						</div>
-					</form>
-				</div>
-			</div>
-		</div>
+                    <div class="form-group">
+                        <button type="submit" class="btn btn-primary" style="width:100%">REGISTER</button>
+                    </div>
+
+                    <div class="form-group form-helper-links">
+                        <a class="pull-right" href="{{ url('/auth/login') }}"><small>Got an account?</small></a>
+                    </div>
+                </form>
+            </div>
+        </div>
 	</div>
 </div>
 @endsection
